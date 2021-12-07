f4364e6ae20c905b7c0819fd6a3ea73ba7ba8c20
NguyenChang21 <Nguyenkieutrang_t65@hus.edu.vn>
2021-12-07 19:28:35 +0700

    thêm thuộc tính, getter and setter

--- mangager_app/src/teacher/Teacher.java
+++ mangager_app/src/teacher/Teacher.java
@@ -1,5 +1,149 @@
 package teacher;
 
 public class Teacher {
+    private String firstname;
+    private String lastname;
+    private String gender;
+    private String email;
+    private String  ethnic_group;
+    private int id_number;
+    private String address;
+    private String religion;
+    private String department;
+    private int phone_number;
+    private String username;
+    private String password;
+    private String status;
+    private String teaching_class;
+    private String qualification;
+    private String extra_notes;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEthnic_group() {
+        return ethnic_group;
+    }
+
+    public void setEthnic_group(String ethnic_group) {
+        this.ethnic_group = ethnic_group;
+    }
+
+    public int getId_number() {
+        return id_number;
+    }
+
+    public void setId_number(int id_number) {
+        this.id_number = id_number;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getReligion() {
+        return religion;
+    }
+
+    public void setReligion(String religion) {
+        this.religion = religion;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(int phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTeaching_class() {
+        return teaching_class;
+    }
+
+    public void setTeaching_class(String teaching_class) {
+        this.teaching_class = teaching_class;
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
+    public String getExtra_notes() {
+        return extra_notes;
+    }
+
+    public void setExtra_notes(String extra_notes) {
+        this.extra_notes = extra_notes;
+    }
 
 }
