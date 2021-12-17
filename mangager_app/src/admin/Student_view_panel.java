5fd9b7e23103801c21051b63f230afd2638b1946
NguyenChang21 <Nguyenkieutrang_t65@hus.edu.vn>
2021-12-17 09:56:20 +0700

    add student view panel

--- mangager_app/src/admin/Student_view_panel.java
+++ mangager_app/src/admin/Student_view_panel.java
@@ -1,10 +1,11 @@
 package admin;
 import prototype.rounded_panel;
-
 import javax.swing.*;
 import java.awt.*;
 
-public class Student_view_panel extends rounded_panel{
+public class Student_view_panel extends rounded_panel  {
+
+
     public Student_view_panel(int radius) {
         super(radius);
     }
@@ -14,52 +15,34 @@ public class Student_view_panel extends rounded_panel{
         GridBagLayout gbl = new GridBagLayout();
         GridBagConstraints gbc = new GridBagConstraints();
         setLayout(gbl);
-
+        gbc.weightx=1;
+        gbc.weighty=1;
         gbc.fill = GridBagConstraints.BOTH;
+        JPanel p1 = new Student_view_toppanel(4,Color.cyan);
+        p1.setBackground(Color.cyan);
 
-        gbc.weighty = 1;
-        gbc.weightx = 1;
+        JPanel p2 = new Student_view_middlepanel(4,Color.cyan);
+        p2.setBackground(Color.blue);
 
-        JPanel p1 = new JPanel();
-        p1.setBackground(Color.YELLOW);
-        JPanel p2 = new JPanel();
-        p2.setBackground(Color.RED);
+
         JPanel p3 = new JPanel();
-        p3.setBackground(Color.BLUE);
-        JPanel p4 = new JPanel();
-        p4.setBackground(Color.GREEN);
-        JPanel cp = new JPanel();
-        cp.setBackground(Color.GRAY);
+        p3.setBackground(Color.cyan);
 
-        gbc.weightx = 0.01;
-        gbc.gridheight = 3;
+        gbc.weighty = 0.08;
+        gbc.gridy = 0;
         gbc.gridx = 0;
-        gbc.gridy = 0;
-        add(p1, gbc);
-
-        gbc.weighty = 0.03;
-        gbc.weightx = 1;
-        gbc.gridheight = 1;
-        gbc.gridx = 1;
-        gbc.gridy = 0;
-        add(p2, gbc);
-
-        gbc.weightx = 0.01;
-        gbc.gridheight = 3;
-        gbc.gridx = 2;
-        gbc.gridy = 0;
-        add(p3, gbc);
+        add(p1,gbc);
 
         gbc.weighty = 1;
-        gbc.weightx = 1;
-        gbc.gridheight = 1;
-        gbc.gridx = 1;
-        gbc.gridy = 1;
-        add(cp, gbc);
+        gbc.gridx =0;
+        gbc.gridy =1;
+        add(p2,gbc);
 
-        gbc.weighty = 0.03;
-        gbc.gridx = 1;
+        gbc.weighty = 0.01;
         gbc.gridy = 2;
-        add(p4, gbc);
+        gbc.gridx = 0;
+        add(p3,gbc);
+
     }
 }
+
